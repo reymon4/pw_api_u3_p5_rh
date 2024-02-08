4a69725aea787d507f99485a0828e29cdb1e8968
Reymon <rahidalgob@uce.edu.ec>
2024-02-07 21:57:47 -0500

    Richardson Maturity Model, Level 2 - Tarea9

--- src/main/java/com/example/demo/controller/ProfesorControllerRestFul.java
+++ src/main/java/com/example/demo/controller/ProfesorControllerRestFul.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,43 +24,47 @@ import com.example.demo.service.IProfesorService;
 @RestController // Servicio
 @RequestMapping(path = "/profesores")
 public class ProfesorControllerRestFul {
-	
+	// PARA PROFESOR USAREMOS LOS CÓDIGOS DESDE EL 260
 	@Autowired
 	private IProfesorService profesorService;
-	
+
 	// http://localhost:8080/API/v1.0/Matricula/profesores
-	
-	@GetMapping(path = "/{id}")
-	public Profesor search(@PathVariable Integer id) {
-		return this.profesorService.search(id);
+
+	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Profesor> search(@PathVariable Integer id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("260 message", "Se ha obtenido de manera exitosa el objeto número"+id);
+		return new ResponseEntity<>(this.profesorService.search(id),headers, 260);
 	}
-	@PostMapping
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void save(@RequestBody Profesor profesor) {
 		this.profesorService.save(profesor);
 	}
 
-	@PutMapping(path = "/{id}")
+	@PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void update(@RequestBody Profesor profesor, @PathVariable Integer id) {
 		profesor.setId(id);
 		this.profesorService.update(profesor);
 	}
 
-	@PatchMapping(path = "/{id}")
-	public void partialUpdate(@RequestBody Profesor profesor,  @PathVariable Integer id) {
+	@PatchMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void partialUpdate(@RequestBody Profesor profesor, @PathVariable Integer id) {
 		this.profesorService.partialUpdate(profesor.getLastName(), profesor.getName(), id, profesor.getEmail());
 	}
 
-	@DeleteMapping(path = "/{id}")
+	@DeleteMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void delete(@PathVariable Integer id) {
 		this.profesorService.delete(id);
 	}
-	
-	
+
 	// http://localhost:8080/API/v1.0/Matricula/estudiantes/searchAll?gender=M&edad=18
-	@GetMapping
-	public List<Profesor> searchAll(@RequestParam(defaultValue = "M",required=false)  String gender) {
-		return this.profesorService.searchAll(gender);
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Profesor>> searchAll(@RequestParam(defaultValue = "M", required = false) String gender) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("261 message", "Lista consultada de manera satisfactoria!");
+		headers.add("info message", "El sistema va a estar en mantenimiento el finde!");
+		return new ResponseEntity<>(this.profesorService.searchAll(gender), headers, 261);
 	}
-	
-	
+
 }
